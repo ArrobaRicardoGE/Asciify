4f22ca65ee11fc944b250481bf04c01958407d47
ArrobaRicardoGE <ricardoarroba@outlook.com>
2020-08-04 10:51:22 -0500

    Built support for paste.ee API

--- PasteAPI.py
+++ PasteAPI.py
@@ -1,13 +1,36 @@
-#Simple script to make a pastw with paste.ee API
+#Integration of the paste.ee API
+#See https://pastee.github.io/docs/ for more information
 
 import requests,json
 
-def createNewPaste(string,name,token):
-    headers = {'Content-Type':'application/json','X-Auth-Token':token}
-    payload = {'sections':[{'name':name, 'syntax':'text','contents':string}]}
-    r = requests.post("https://api.paste.ee/v1/pastes",headers=headers,data=json.dumps(payload))
-    if('201' in str(r)):
-        return json.loads(r.text)["link"]
-    else:
-        return ""
+class API:
+    def __init__(self,token):
+        self.token = token
+    
+    def createNewPaste(self,string,name):
+        headers = {'Content-Type':'application/json','X-Auth-Token':self.token}
+        payload = {'sections':[{'name':name, 'syntax':'text','contents':string}]}
+        r = requests.post("https://api.paste.ee/v1/pastes",headers=headers,data=json.dumps(payload))
+        if(not '201' in str(r)): raise Exception('Unable to process the requests: '+str(r))
+
+    def retrieveLastId(self):
+        headers = {'X-Auth-Token':self.token}
+        id = self.getLastPaste()
+        r = requests.get('https://api.paste.ee/v1/pastes/'+id,headers = headers)
+        if(not '200' in str(r)): raise Exception('Unable to process the requests: '+str(r))
+        return json.loads(r.text)['paste']['sections'][0]['contents']
+
+    def deleteLastPaste(self):
+        headers = {'X-Auth-Token':self.token}
+        id = self.getLastPaste()
+        r = requests.delete('https://api.paste.ee/v1/pastes/'+id,headers = headers)
+        if(not '200' in str(r)): raise Exception('Unable to process the requests: '+str(r))
+
+    def getLastPaste(self):
+        headers = {'X-Auth-Token':self.token}
+        r = requests.get('https://api.paste.ee/v1/pastes',headers=headers)
+        if(not '200' in str(r)): raise Exception('Unable to process the requests: '+str(r))
+        d = json.loads(r.text)
+        if(len(d['data']) == 0):raise Exception('No pastes available')
+        return d['data'][0]['id']
 
